1009284f06d16afafe64977fefc45e5828d0288b
Julien Ledieu <jledieu@mespaysans.com>
2016-06-14 11:22:50 +0200

    Ajout du repertoire en_construction

---
+++ en_construction/index_alter_1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>mespaysans.com</title>
+        <style>
+            body {width:800px; font-family: "Arial"; font-size: 22px;margin: auto;}
+            p {color:blue;}
+            .logo {
+            	margin-bottom: 100px;
+            	text-align: center;
+            }
+            .annonce {
+            	float :left;
+            	width: 350px;
+            	font-style: italic;
+            	text-align: center;
+            	margin-bottom : 100px;
+            }
+            .top {
+            	margin-top : 40px;
+            }
+            .left {
+            	margin-right: 100px; 
+            }
+            .texte {
+            	clear: left;
+            	text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="annonce left top">
+        "Une nouvelle façon d'aller au marché."
+        </div>
+        <div class="annonce top">
+        "C'est facile ! Je commande en ligne puis je retire sur place"
+        </div>
+        <div class="logo">
+        	<img src="logo.jpg" alt="mespaysans.com" />
+        </div>
+        <div class="texte">
+        	Vos paysans installent actuellement leurs produits sur leurs emplacements du site.
+        	<br />
+        	Vous les retrouverez très prochainement.
+        </div>
+    </body>
+</html>
